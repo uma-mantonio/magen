e19ac0bf5affc84b0e4c1bd2cb486d2856f47bd9
uma-mantonio <mantonio@umasolar.com>
2014-10-13 09:30:29 -0400

    Added videoBG.js and script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#intro').videoBG({
+		position: 'fixed',
+		zIndex: 0,
+		mp4: 'assets/magen.mp4',
+		ogv: 'assets/magen.ogv',
+		webm: 'assets/magen.webm',
+		poster: 'images/magen-poster.jpg',
+		opacity: 1
+	});
+})
