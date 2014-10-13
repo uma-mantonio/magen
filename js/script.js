ac74c7d8837ae861df29bb8126c0ea12b9812cbe
uma-mantonio <mantonio@umasolar.com>
2014-10-13 12:55:02 -0400

    set assembly video to stay within a section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$('#assembly-vid').videoBG({
-		position: 'fixed',
+		// position: 'fixed',
 		zIndex: 0,
 		mp4: 'assets/magen.mp4',
 		ogv: 'assets/magen.ogv',
 		webm: 'assets/magen.webm',
 		poster: 'images/magen-poster.jpg',
 		loop: true,
-		fullscreen: true,
+		scale: true,
 		opacity: 1
 	});
 })
